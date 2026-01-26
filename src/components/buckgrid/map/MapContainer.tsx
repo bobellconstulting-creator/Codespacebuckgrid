57375845897516c7ece03f8eab8c5667e07e1550
bobellconstulting-creator <bobellconstulting@gmail.com>
2026-01-25 19:47:26 -0600

    Add MapContainer component for map drawing

---
+++ src/components/buckgrid/map/MapContainer.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React, { forwardRef, useImperativeHandle, useRef } from 'react'
+import type { Tool } from '../constants/tools'
+import { useMapDrawing, type MapApi } from '../hooks/useMapDrawing'
+
+export type MapContainerHandle = MapApi
+
+const MapContainer = forwardRef<MapContainerHandle, { activeTool: Tool, brushSize: number }>(({ activeTool, brushSize }, ref) => {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const { api, handlers } = useMapDrawing({ containerRef, activeTool, brushSize })
+  useImperativeHandle(ref, () => api, [api])
+
+  return (
+    <div
+      ref={containerRef}
+      onPointerDown={handlers.onPointerDown}
+      onPointerMove={handlers.onPointerMove}
+      onPointerUp={handlers.onPointerUp}
+      style={{ height: '100%', width: '100%', zIndex: 1, touchAction: 'none', background: '#000' }}
+    />
+  )
+})
+
+export default React.memo(MapContainer)
